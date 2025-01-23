e0db2139ad2483f6771f7704cb1ac8d7670ad9d4
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-01-23 20:14:55 +1100

    Add main elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,73 @@
+import {useState} from "react"
+import { languages } from "./languages"
 function AssemblyEndgame() {
 
-  return (
+  const [currentWord, setCurrentWord] = useState("react")
+    const [guessedLetters, setGuessedLetters] = useState([])
+    console.log(guessedLetters)
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters => 
+            prevLetters.includes(letter) ? 
+                prevLetters : 
+                [...prevLetters, letter]
+        )
+    }
+
+    const languageElements = languages.map(lang => {
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        return (
+            <span
+                className="chip"
+                style={styles}
+                key={lang.name}
+            >
+                {lang.name}
+            </span>
+        )
+    })
+
+    const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>{letter.toUpperCase()}</span>
+    ))
+
+    const keyboardElements = alphabet.split("").map(letter => (
+        <button
+            key={letter}
+            onClick={() => addGuessedLetter(letter)}
+        >
+            {letter.toUpperCase()}
+        </button>
+    ))
+
+    return (
         <main>
             <header>
                 <h1>Assembly: Endgame</h1>
-                <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+                <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
             </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
+            </section>
+            <section className="word">
+                {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
+            </section>
+            <button className="new-game">New Game</button>
         </main>
-  )
+    )
 }
 
 export default AssemblyEndgame
